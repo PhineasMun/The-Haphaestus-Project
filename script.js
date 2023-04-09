1d392fdd807d58dc006a577013156fbeca916df1
rpixpunk <79791897+rpixpunk@users.noreply.github.com>
2023-04-09 12:26:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function showDivs(n){
   }
   x[slideIndex-1].style.display = "block";
 }
+
+let map;
+      function initMap(){
+          let tdad = {lat: 10.52299, lng:-61.27558};
+          map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10, 
+            center: tdad,
+            disableDefaultUI: false,
+            zoomControl: false,
+            styles: [    
+            {
+              featureType: "all",
+              elementType: "labels",
+              stylers: [{ visibility: "off" }]
+            }
+          ]
+      });
+            
+      addMarker({lat: 10.52299, lng: -61.27558});
+      addMarker({lat: 10.66043426739283, lng: -61.51246341723815});
+      addMarker({lat: 10.443972705204382, lng: -61.45050099115099});
+      addMarker({lat: 10.282112331842258, lng: -61.46359416415093})
+
+      function addMarker(coords){
+          let marker = new google.maps.Marker({
+            position: coords, 
+            map: map
+          });
+      }
+      }
